567aae5275a4b27c015e029d52dc64d9b1fc3311
Sublio <sublio1260@gmail.com>
2016-08-10 21:23:52 +0600

    DataManager

---
+++ CoreDataTest/DMDataManager.h
@@ -0,0 +1,13 @@
+//
+//  DMDataManager.h
+//  CoreDataTest
+//
+//  Created by sublio on 10/08/16.
+//  Copyright (c) 2016 Alex Skutarenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMDataManager : NSObject
+
+@end
---
+++ CoreDataTest/DMDataManager.m
@@ -0,0 +1,13 @@
+//
+//  DMDataManager.m
+//  CoreDataTest
+//
+//  Created by sublio on 10/08/16.
+//  Copyright (c) 2016 Alex Skutarenko. All rights reserved.
+//
+
+#import "DMDataManager.h"
+
+@implementation DMDataManager
+
+@end
